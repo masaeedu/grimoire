f51f7377b90033bcec51af697fe5e1aa848fb7e4
Asad Saeeduddin <masaeedu@gmail.com>
2021-05-02 12:15:21 -0400

    Add some functors

---
+++ lib/Data/Typelevel/Setter.hs
@@ -0,0 +1,14 @@
+module Data.Typelevel.Setter where
+
+import Data.Typelevel.Function
+
+type Setter s t a b = (a ⇒ b) -> s ⇒ t
+
+type CFunctor :: Setter (f a) (f b) a b
+class CFunctor c fa fb
+
+instance CFunctor c 'Nothing 'Nothing
+instance c a b => CFunctor c ('Just a) ('Just b)
+
+instance CFunctor c '[] '[]
+instance c x y => CFunctor c (x ': xs) (y ': ys)
