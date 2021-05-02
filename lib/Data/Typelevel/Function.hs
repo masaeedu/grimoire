0d5e2d4585528a58711a2a102390db453c57cb30
Asad Saeeduddin <masaeedu@gmail.com>
2021-05-02 13:53:12 -0400

    Variables, naming

--- lib/Data/Typelevel/Function.hs
+++ lib/Data/Typelevel/Function.hs
@@ -13,15 +13,17 @@ import GHC.TypeLits
 
 -- A nethereal profunctor enriched in the category of types and relations
 type a ⇒ b = a -> b -> Constraint
+infixr 0 ⇒
 
 -- An earthly profunctor enriched in the category of types and functions
 type a → b = a -> b -> Type
+infixr 0 →
 
 -- The result of evaluating a single type in the domain of a functional relation
 type CCase :: a ⇒ b -> a ⇒ b
 class (CFunction c, c a b) => CCase c a b | c a -> b
 
--- This, so we can write: `Not @ 'False := 'True`
+-- This, so we can write `CCase Not 'False 'True` as `Not @ 'False := 'True`
 type (^@) = CCase
 infix 9 ^@
 type (:=) fx = fx
@@ -36,8 +38,8 @@ type CId :: x ⇒ x
 instance CId a a
 
 -- NB: We have to do this hemming and hawing for all functional relations
-class CId a b | a -> b
-instance CId a b => CCase CId a b
+class CId i o | i -> o
+instance CId i o => CCase CId i o
 instance CFunction CId
 
 -- Composition
@@ -45,8 +47,8 @@ type (∘) :: (b ⇒ c) -> (a ⇒ b) -> a ⇒ c
 instance (g ^@ a := x, f ^@ x := b) => (f ∘ g) a b
 
 -- *cough, cough*
-class (f ∘ g) a b | f g a -> b
-instance (f ∘ g) a b => CCase (f ∘ g) a b
+class (f ∘ g) i o | f g i -> o
+instance (f ∘ g) i o => CCase (f ∘ g) i o
 instance CFunction (f ∘ g)
 
 -- A nethereal journey from point A to point B
@@ -63,14 +65,14 @@ instance Category Dream
   id = dream @CId
   Dream (_ :: Proxy f) . Dream (_ :: Proxy g) = Dream $ Proxy @(f ∘ g)
 
--- Test 1
+-- Test 1: Basic unary function
 type Not :: Bool ⇒ Bool
 instance Not 'False 'True
 instance Not 'True 'False
 
 -- *hack, hack*
-class Not a b | a -> b
-instance Not a b => CCase Not a b
+class Not i o | i -> o
+instance Not i o => CCase Not i o
 instance CFunction Not
 
 test1_1 :: Dream 'False _ -- = 'True :: Bool
@@ -79,13 +81,13 @@ test1_1 = dream @Not
 test1_2 :: Dream 'False _ -- = 'False :: Bool
 test1_2 = dream @Not . dream @Not
 
--- Test 2
+-- Test 2: Basic binary function, no currying/HOF
 type Append :: (Symbol, Symbol) ⇒ Symbol
 instance AppendSymbol a b ~ c => Append '(a, b) c
 
 -- etc. etc.
-class Append s1s2 r | s1s2 -> r
-instance Append s1s2 r => CCase Append s1s2 r
+class Append i o | i -> o
+instance Append i o => CCase Append i o
 instance CFunction Append
 
 test2_1 :: Dream '("hello, ", "world") _ -- = "hello, world" :: Symbol
