88dc19900983baa7ec61054652602477e240bd59
Asad Saeeduddin <masaeedu@gmail.com>
2021-05-02 12:11:23 -0400

    Make things nicer and more principled

--- lib/Data/Typelevel/Function.hs
+++ lib/Data/Typelevel/Function.hs
@@ -2,64 +2,77 @@
 {-# LANGUAGE UndecidableInstances #-}
 {-# LANGUAGE UndecidableSuperClasses #-}
 {-# LANGUAGE PartialTypeSignatures #-}
+{-# LANGUAGE AllowAmbiguousTypes #-}
 module Data.Typelevel.Function where
 
-import Prelude hiding (not)
+import Prelude hiding (not, id, (.))
 import Data.Kind
 import Data.Proxy
+import Control.Category
 
--- The kind-level profunctor of relations
+-- A nethereal profunctor enriched in the category of types and relations
 type a ⇒ b = a -> b -> Constraint
 
--- The result of evaluating a single type in the domain of a in a functional relation
+-- An earthly profunctor enriched in the category of types and functions
+type a → b = a -> b -> Type
+
+-- The result of evaluating a single type in the domain of a functional relation
 type CCase :: a ⇒ b -> a ⇒ b
 class (CFunction c, c a b) => CCase c a b | c a -> b
 
--- A functional relation (i.e. one for which the domain value uniquely determines the result, over the entire domain)
+-- This, so we can write: `Not @ 'False := 'True`
+type (^@) = CCase
+infix 9 ^@
+type (:=) fx = fx
+infix 0 :=
+
+-- A functional relation (i.e. one for which every type in the domain kind uniquely determines a result type)
 type CFunction :: (a ⇒ b) -> Constraint
-class (forall x y. c x y => CCase c x y) => CFunction c
+class (forall x y. c x y => c ^@ x := y) => CFunction c
 
--- The earthly entrance to a nethereal journey
-type Dream :: (a ⇒ b) -> Type
-data Dream c
-  where
-  Dream :: CFunction c => Dream c
+-- The identity functional relation
+type CId :: x ⇒ x
+instance CId a a
 
--- Evaluation
-(^$) :: c x y => Dream (c :: a ⇒ b) -> Proxy x -> Proxy y
-(^$) Dream Proxy = Proxy
-
-infixr 0 ^$
+-- NB: We have to do this hemming and hawing for all functional relations
+class CId a b | a -> b
+instance CId a b => CCase CId a b
+instance CFunction CId
 
 -- Composition
 type (∘) :: (b ⇒ c) -> (a ⇒ b) -> a ⇒ c
+instance (g ^@ a := x, f ^@ x := b) => (f ∘ g) a b
+
+-- *cough, cough*
 class (f ∘ g) a b | f g a -> b
-
-infixr 9 ∘
-
-instance (CCase f x b, CCase g a x) => (f ∘ g) a b
-
--- Witness that composition forms a function
 instance (f ∘ g) a b => CCase (f ∘ g) a b
 instance CFunction (f ∘ g)
 
--- Earthly co-avatar of nethereal composition
-(∘) :: (CFunction c, CFunction d) => Dream c -> Dream d -> Dream (c ∘ d)
-Dream ∘ Dream = Dream
+-- A nethereal journey from point A to point B
+data Dream a b
+  where
+  Dream :: (c ^@ a := b) => Proxy c -> Dream a b
+
+dream :: forall c x y. (c ^@ x := y) => Dream x y
+dream = Dream $ Proxy @c
+
+instance Category Dream
+  where
+  id = dream @CId
+  Dream (_ :: Proxy f) . Dream (_ :: Proxy g) = Dream $ Proxy @(f ∘ g)
 
 -- Test
 type Not :: Bool ⇒ Bool
-class Not a b | a -> b
-
--- We have to do this hemming and hawing for all functional relations
-instance Not a b => CCase Not a b
-instance CFunction Not
-
 instance Not 'False 'True
 instance Not 'True 'False
 
-test1 :: Proxy _ -- GHC says "Found wildcard standing for 'True :: Bool"
-test1 = Dream @Not ^$ Proxy @'False
+-- *hack, hack*
+class Not a b | a -> b
+instance Not a b => CCase Not a b
+instance CFunction Not
 
-test2 :: Proxy _ -- GHC says "Found wildcard standing for 'False :: Bool"
-test2 = Dream @Not ∘ Dream @Not ^$ Proxy @'False
+test1 :: Dream 'False _ -- GHC says "Found wildcard standing for 'True :: Bool"
+test1 = dream @Not
+
+test2 :: Dream 'False _ -- GHC says "Found wildcard standing for 'False :: Bool"
+test2 = dream @Not . dream @Not
